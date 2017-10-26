a16c966519063eff4749b73bdf91d927ecc82fd1
Quy Nguyen <quypng@gmail.com>
2017-10-25 21:55:49 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import Projects from './components/Projects'
+import AddProject from './components/AddProject'
+import './App.css';
+
+class App extends Component {
+  // Constructor
+  constructor() {
+    super();
+    this.state = {
+      projects: []
+    }
+  }
+  // Life Cycle Method
+  componentWillMount() {
+    this.setState({
+      projects: [
+        {
+          title: 'Business Website',
+          category: 'Web Design'
+        }, {
+          title: 'Social App',
+          category: 'Mobile Development'
+        }, {
+          title: 'E-commerce Shopping Cart',
+          category: 'Web Development'
+        }
+      ]
+    });
+  }
+
+  handleAddProject(project) {
+    // console.log(project);
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects: projects});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <AddProject
+          addProject={this
+          .handleAddProject
+          .bind(this)}/>
+        <Projects projects={this.state.projects}/>
+      </div>
+    );
+  }
+}
+
+export default App;
